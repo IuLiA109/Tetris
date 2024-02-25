bead4bec58c152f3df76060f2d7860e118f2ea27
Tamas Iulia <iuliatamas03@gmail.com>
2024-02-25 18:53:52 +0200

    done

--- TetrisGame/main.cpp
+++ TetrisGame/main.cpp
@@ -7,12 +7,19 @@ using namespace sf;
 
 int forms[7][4] = { {0,2,4,6}, {4,1,3,5}, {0,2,4,5}, {0,2,1,3}, {0,2,3,5}, {2,1,3,5}, {2,4,1,3} };
 
-int l[4], c[4],linit[4], cinit[4]; // l ~ y/height   c ~ x/width
+enum app_state {
+    playing,
+    over
+};
+
+int l[4], c[4],linit[4], cinit[4]; 
+// l ~ y/height   c ~ x/width
 const int n = 10; //nr of columns
 const int m = 18; //nr of lines
 int matrix[m][n];
 int points;
 
+//-------------------HELPFUL FUNCTIONS--------------------------
 bool check_collision() {
     for (int i = 0; i < 4; i++) {
         if (c[i] < 0 || c[i] >= n || l[i] < 0 || l[i] >= m) return true;
@@ -37,11 +44,7 @@ void init_game() {
 
     points = 0;
 }
-
-enum app_state {
-    playing,
-    over
-};
+//-------------------HELPFUL FUNCTIONS--------------------------
 
 int main()
 {
@@ -52,8 +55,10 @@ int main()
 
     int window_w = (n * 2) * 32;
     int window_h = (m * 5 / 4) * 32;
+
     RenderWindow window(sf::VideoMode(window_w, window_h), "TETRIS");
 
+    //-------------------TEXTURES AND FONTS--------------------------
     Texture texture;
     if (!texture.loadFromFile("tetris_resurse/squares2.png")) {
         return EXIT_FAILURE;
@@ -64,6 +69,14 @@ int main()
         return EXIT_FAILURE;
     }
 
+    Font font;
+    if (!font.loadFromFile("tetris_fonts/arial.ttf")) {
+        return 1;
+    }
+    //-------------------TEXTURES AND FONTS--------------------------
+
+
+    //----------------------SPRITES-------------------------------
     Sprite sprite;
     sprite.setTexture(texture);
     sprite.setPosition(100, 100);
@@ -71,21 +84,22 @@ int main()
     Sprite white_sprite;
     white_sprite.setTexture(white_block);
     white_sprite.setTextureRect(IntRect(1, 1, 38, 38));
+    //----------------------SPRITES-------------------------------
 
-    RectangleShape boarder;
-    boarder.setSize(Vector2f(n*32-6, m*32-6));
-    boarder.setPosition(3+32, 3+32);
-    boarder.setFillColor(Color::White);
-    boarder.setOutlineThickness(3);
-    boarder.setOutlineColor(Color::Black);
+
+    //----------------------BORDER-------------------------------
+    RectangleShape border;
+    border.setSize(Vector2f(n*32-6, m*32-6));
+    border.setPosition(3+32, 3+32);
+    border.setFillColor(Color::White);
+    border.setOutlineThickness(3);
+    border.setOutlineColor(Color::Black);
+    //----------------------BORDER-------------------------------
 
     Clock clock;
     float delay = 0.3, timer = 0;
 
-    Font font;
-    if (!font.loadFromFile("tetris_fonts/arial.ttf")) { 
-        return 1;
-    }
+    //----------------------SCORE-------------------------------
     Text score;
     points = 0;
     score.setFont(font);
@@ -95,7 +109,10 @@ int main()
     score.setPosition((n * 3 / 2) * 32 + 16 , (m / 5) * 32 + 32);
     FloatRect scoreBounds = score.getLocalBounds();
     score.setOrigin(scoreBounds.left + scoreBounds.width / 2, scoreBounds.top + scoreBounds.height / 2);
+    //----------------------SCORE-------------------------------
 
+
+    //----------------------NEXT SHAPE-------------------------------
     Text next_form;
     next_form.setFont(font);
     next_form.setString("NEXT SHAPE:");
@@ -104,9 +121,11 @@ int main()
     next_form.setPosition((n * 3 / 2) * 32 + 16, (m / 3) * 32);
     FloatRect next_formBounds = next_form.getLocalBounds();
     next_form.setOrigin(next_formBounds.left + next_formBounds.width / 2, next_formBounds.top + next_formBounds.height / 2);
+    //----------------------NEXT SHAPE-------------------------------
 
     init_game();
 
+    //----------------------GAME OVER-------------------------------
     Text gameOver;
     gameOver.setFont(font);
     gameOver.setString("Game Over!\nScore: " + to_string(points));
@@ -116,7 +135,10 @@ int main()
     FloatRect textBounds = gameOver.getLocalBounds();
     gameOver.setOrigin(textBounds.left + textBounds.width / 2, textBounds.top + textBounds.height / 2);
     gameOver.setPosition(window.getSize().x / 2, window.getSize().y / 2 - 50);
+    //----------------------GAME OVER-------------------------------
 
+
+    //----------------------RESTART-------------------------------
     RectangleShape restart_button;
     restart_button.setSize(Vector2f(120, 65));
     restart_button.setPosition(window.getSize().x / 2, window.getSize().y / 2 + window_h * 7 / 100);
@@ -126,13 +148,13 @@ int main()
     Vector2f restart_button_size = restart_button.getSize();
     restart_button.setOrigin(restart_button_size.x / 2, restart_button_size.y / 2);
 
-
     Text buttonText("Restart", font, 27);
     buttonText.setFillColor(Color::Black);
 
     FloatRect buttonTextBounds = buttonText.getLocalBounds();
     buttonText.setOrigin(buttonTextBounds.left + buttonTextBounds.width / 2, buttonTextBounds.top + buttonTextBounds.height / 2);
     buttonText.setPosition(restart_button.getPosition().x, restart_button.getPosition().y);
+    //----------------------RESTART-------------------------------
 
     app_state current_app_state = app_state::playing;
 
@@ -144,11 +166,13 @@ int main()
 
         sf::Event event;
         int no = 0;
+
         while (window.pollEvent(event))
         {
             if (event.type == Event::Closed)
                 window.close();
 
+            //ARROWS
             if (event.type == Event::KeyPressed) {
                 if (event.key.code == Keyboard::Right) move = 1;
                 if (event.key.code == Keyboard::Left) move = -1;
@@ -156,9 +180,10 @@ int main()
                 if (event.key.code == Keyboard::Down) delay = 0.05;
             }
 
-            if (current_app_state == app_state::over and event.type == sf::Event::MouseButtonReleased) {
-                if (event.mouseButton.button == sf::Mouse::Left) {
-                    sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+            //RESTART BUTTON
+            if (current_app_state == app_state::over and event.type == Event::MouseButtonReleased) {
+                if (event.mouseButton.button == Mouse::Left) {
+                    Vector2f mousePos = window.mapPixelToCoords(Mouse::getPosition(window));
                     if (restart_button.getGlobalBounds().contains(mousePos)) {
                         init_game();
                         current_app_state = app_state::playing;
@@ -168,6 +193,7 @@ int main()
             }
         }
 
+        //GAME OVER
         if (current_app_state == app_state::over) {
 
             score.setString("SCORE: 0");
@@ -177,15 +203,17 @@ int main()
             window.draw(buttonText);
             window.display();
         }
+
+        //PLAYING 
         if (current_app_state == app_state::playing) {
 
-            // init
+            //save init values
             for (int i = 0; i < 4; i++) {
                 linit[i] = l[i];
                 cinit[i] = c[i];
             }
 
-            //move
+            //move left right
             for (int i = 0; i < 4; i++)
                 c[i] += move;
 
@@ -212,6 +240,7 @@ int main()
             }
 
             //if collision then back to init
+            //no movement if collision
             if (check_collision()) {
                 for (int i = 0; i < 4; i++) {
                     c[i] = cinit[i];
@@ -220,17 +249,17 @@ int main()
             }
 
             if (timer > delay) {
+                //move dowm and save init values
                 for (int i = 0; i < 4; i++) {
                     linit[i] = l[i];
                     cinit[i] = c[i];
-                    l[i]++; //move dowm
+                    l[i]++; 
                 }
 
                 if (check_collision()) {
                     for (int i = 0; i < 4; i++)
                         matrix[linit[i]][cinit[i]] = color;
 
-                    //nform = rand() % 6;
                     nform = nextform;
                     nextform = rand() % 6;
                     color = (color + 1) % 5;
@@ -243,6 +272,7 @@ int main()
                 timer = 0;
             }
 
+            //first form
             if (first) {
                 nform = rand() % 6;
                 nextform = rand() % 6;
@@ -279,16 +309,19 @@ int main()
             first = 0;
             delay = 0.3;
 
+            //test game over
             if (game_over()) {
                 gameOver.setString("Game Over!\nScore: " + to_string(points));
                 current_app_state = app_state::over;
             }
 
+            //----------------------DRAW-------------------------------
             window.clear(Color::White);
-            window.draw(boarder);
+            window.draw(border);
             window.draw(score);
             window.draw(next_form);
 
+            //fixed forms
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
                     if (matrix[i][j] != -1) {
@@ -299,19 +332,21 @@ int main()
                 }
             }
 
+            //moving form
             for (int i = 0; i < 4; i++) {
                 sprite.setTextureRect(IntRect(30 * color, 0, 32, 32));
                 sprite.setPosition(c[i] * 32 + 32, l[i] * 32 + 32);
                 window.draw(sprite);
             }
 
-            //next shape
+            //next form
             for (int i = 0; i < 4; i++) {
                 white_sprite.setPosition(forms[nextform][i] % 2 * 36 + (n * 3 / 2 * 32), forms[nextform][i] / 2 * 36 + (m / 3 * 32) + 32);
                 window.draw(white_sprite);
             }
 
             window.display();
+            //----------------------DRAW-------------------------------
         }
     }
 
